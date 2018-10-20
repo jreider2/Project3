389a4995c6988c2227c315c56a7cc59b3bdf50f9
jreider2 <julesreider@gmail.com>
2018-10-20 10:42:29 -0500

    moved project 3 Main method.

--- src/client/Project3Main.java
+++ src/client/Project3Main.java
@@ -1,6 +1,7 @@
+package client;
 import partner.PartnerManager;
 
-public class Project3TestMain {
+public class Project3Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
