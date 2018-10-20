32544c14c0dd712e83da39624bf0e9ca762ea26a
jreider2 <julesreider@gmail.com>
2018-10-20 10:37:41 -0500

    added a main for testing service layer.

---
+++ src/Project3TestMain.java
@@ -0,0 +1,12 @@
+import partner.PartnerManager;
+
+public class Project3TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PartnerManager partManager = new PartnerManager();
+		partManager.getAllPartners();
+
+	}
+
+}
