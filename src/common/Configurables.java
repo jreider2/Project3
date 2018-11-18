3d3df3308fc8fd5c098018096a0ef2b5a4a8255d
jreider2 <julesreider@gmail.com>
2018-11-17 18:49:28 -0600

    added a confingurables file in order to reuse and edit links more easily.

---
+++ src/common/Configurables.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package common;
+
+/**
+ * @author julianareider
+ *
+ */
+public class Configurables {
+	
+	public static final String STATUS_URL = "http://localhost:8081/order/orderService/status/{orderID}";
+	public static final String CANCEL_URL = "http://localhost:8081/order/orderService/order/cancelledorder/{orderID}";
+	public String SEARCH_URL = "http://localhost:8081/productservice/products/searchresults/{searchterm}";
+	public String BUY_URL = "http://localhost:8081/order/orderService/order/neworder";
+	
+	/**
+	 * 
+	 */
+	public Configurables() {
+		
+		// TODO Auto-generated constructor stub
+	}
+
+}
