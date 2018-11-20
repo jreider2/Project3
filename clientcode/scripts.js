2c1c6099cd15ebc3d5483542f6452e9b653232ef
AJRohrer <andrewjrohrer@gmail.com>
2018-11-20 12:00:00 -0600

    client changes

--- clientcode/scripts.js
+++ clientcode/scripts.js
@@ -4,5 +4,15 @@ $(document).ready(function(){
         $.ajax({url: "http://localhost:8081/creditcard/customercard/" + $("#creditcardno").val(), success: function(result){
             $("#creditcardinfo").html(result);
         }});
+    });
+
+    $("#searchbtn").on("click", function(){
+        $.get({url: "http://localhost:8081/productservice/products/searchresults/" + $("#searchterm").val(), success: function(result){
+            $("#creditcardinfo").html(result);
+            for (var i = 0; i < result.length-1;i++){
+                var searchresult = result[i];
+                console.log(searchresult);
+            }
+        }});
     })
 });
