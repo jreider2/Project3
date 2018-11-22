dec3f569821d002221a35b1a5b5e9fdef81da3b2
AJRohrer <andrewjrohrer@gmail.com>
2018-11-21 19:45:58 -0600

    Update client code.

--- clientcode/scripts.js
+++ clientcode/scripts.js
@@ -17,31 +17,60 @@ $(document).ready(function(){
                 console.log(searchresult);
             }
         }});
+        $("#searchresults").append(addToSearchResults(5, 5.00))
     });
 
-    $("#btnadd1").on("click", function(){
-        $("#cartItemsList").append(addCartItem(3,3.00));
+    $(".ajr-green-btn").on("click", function(){
+        var btnitemnum = this.attributes[itemno];
+        var btnitemprice = this.attributes[itemprice];
+        var newitem = {number:btnitemnum, price:btnitemprice,qty:1};
+        addToCartArray(newitem);
+        $("#cartItemsList").append(addCartItem(newitem));
+        
     });
 });
 
-function addCartItem(itemNumber, itemPrice) {
+function addCartItem(newCartItem) {
     var htmlitem = "";
+    addToCartArray(newCartItem);
 
     htmlitem = `<article class="ajr-item-100">
                     <section class="ajr-container">
-                        <article class="ajr-inner-item">
-                            Item Number
-                        </article>
-                        <article class="ajr-inner-item">
-                            Item Price
-                        </article>
-                        <article class="ajr-inner-item">
-                            Qty Ordered
-                        </article>
+                        <article class="ajr-inner-item">` +
+                        newCartItem.number +
+                        `</article>
+                        <article class="ajr-inner-item">` +
+                        newCartItem.price +
+                        `</article>
+                        <article class="ajr-inner-item">` + 
+                            calculateQty(newCartItem.number);
+                        `</article>
                     </section>
                 </article>`;
 
     return htmlitem;
+}
+
+function addToCartArray(item){
+    var alreadyExists = false;
+    cartItems.forEach(function(ele){
+        if (ele.number == item.number){
+            alreadyExists = true;
+            ele.qty++;
+        }
+    });
+    if (alreadyExists == false){
+        cartItems.push(item);
+    }
+}
+
+function calculateQty(itemNo){
+    cartItems.forEach(function(ele){
+        if (ele.number == itemNo){
+            return ele.qty;
+        }
+    });
+    return -1; //if the product wasn't found there is a problem.
 }
 
 function addToSearchResults(itemNumber, itemPrice){
